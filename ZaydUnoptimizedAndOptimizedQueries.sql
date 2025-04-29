aeb36df1559958a117f18cec412015e0d85d8a67
ZAYD1SDABEST <98436113+ZAYD1SDABEST@users.noreply.github.com>
2025-04-29 13:45:13 -0700

    Update ZaydUnoptimizedAndOptimizedQueries.sql

--- ZaydUnoptimizedAndOptimizedQueries.sql
+++ ZaydUnoptimizedAndOptimizedQueries.sql
@@ -1,34 +1,52 @@
---Unoptimized Query 1 (I/O cost: 35.18 MB)
+--Unoptimized Query 1 (I/O cost: 67.39 MB)
 --Player's team name, player full name, player points, and minuted displayed where player scored more than one point, was a starter, 
 --and played for more than 30 minutes in a game:
+--ordered by full name
 DECLARE var_min_points INT64;
 SET var_min_points = 1;
-SELECT team_name, full_name, points, minutes
-FROM `bigquery-public-data.ncaa_basketball.mbb_players_games_sr`
+SELECT a.team_name, a.full_name, a.points, a.minutes
+FROM `bigquery-public-data.ncaa_basketball.mbb_players_games_sr` AS a
+JOIN `bigquery-public-data.ncaa_basketball.mbb_players_games_sr` AS b
+  ON a.full_name = b.full_name
+  AND a.game_id = b.game_id
 WHERE
-  points>=var_min_points
-  AND starter = TRUE
-  AND minutes_int64>30
---Optimized Query 1 (I/O cost: 31.38 MB)
+  a.points >= var_min_points
+  AND a.starter = TRUE
+  AND a.minutes_int64 > 30
+GROUP BY
+  a.team_name, a.full_name, a.points, a.minutes
+ORDER BY
+  a.full_name
+--Optimized Query 1 (I/O cost: 35.18 MB)
 --Player's team name, player full name, player points, and minuted displayed where player scored more than one point, was a starter, 
 --and played for more than 30 minutes in a game:
-DECLARE var_min_points INT64 DEFAULT 1;
-DECLARE var_min_minutes INT64 DEFAULT 30;
-WITH minimized_data AS(
-  SELECT team_name, full_name, points, minutes_int64
+--ordered by full name
+DECLARE var_min_points INT64;
+SET var_min_points = 1;
+WITH filtered_out_data AS (
+  SELECT team_name, full_name, points, minutes
   FROM `bigquery-public-data.ncaa_basketball.mbb_players_games_sr`
-  WHERE starter = TRUE
-    AND minutes_int64 > var_min_minutes
-    AND points>=var_min_points
-)
-SELECT * FROM minimized_data;
---Unoptimized/Optimized Query 2 (I/O cost: 40.57 MB)
+  WHERE
+    points >= var_min_points
+    AND starter = TRUE
+    AND minuted_int64 > 30
+  )
+  SELECT f.team_name, f.full_name, f.points, f.minutes
+  FROM filtered_out_data f
+  LEFT JOIN UNNEST([1]) AS j ON TRUE
+  GROUP BY f.team_name, f.full_name, f.points, f.minutes
+  ORDER BY f.full_name
+--Unoptimized/Optimized Query 2 (I/O cost: 72.78 MB)
 --Player's team name, full name, points, field goal percentage, field goals attempted, and threes attempted displayed
 -- for player's who shot more than 50% from the field on 10+ field goal attempts, and 4+ three point attempts
-SELECT team_name, full_name, points, field_goals_pct, field_goals_att, three_points_att
-FROM `bigquery-public-data.ncaa_basketball.mbb_players_games_sr`
-WHERE field_goals_pct>50 AND field_goals_att>10 AND three_points_att>4
-
+--ordered by full name
+SELECT a.team_name, a.full_name, a.points, a.field_goal_pct, a.field_goals_att, a.three_points_att
+FROM `bigquery-public-data.ncaa_basketball.mbb_players_games_sr` AS a
+JOIN `bigquery-public-data.ncaa_basketball.mbb_players_games_sr` AS b
+  ON a.full_name = b.full_name AND a.game_id = b.game_id
+WHERE a.field_goals_pct > 50, AND a.field_goals_att > 10, AND a.three_points_att > 4
+GROUP BY a.team_name, a.full_name, a.points, a.field_goals_pct, a.field_goals_att, a.three_points_att
+ORDER BY a.full_name;
 
 
   
